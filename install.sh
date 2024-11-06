60df735c643a08ce28941f3b632ef45311c725f0
algarrig <algarrig@c1r3s1.42london.com>
2024-11-06 21:05:13 +0000

    add error checking

--- install.sh
+++ install.sh
@@ -1,22 +1,36 @@
 #!/bin/bash
 
 HBDIR="$HOME/homebrew"
-SHELLRC="$HOME/.zshrc"
-SHELL="zsh"
+SHELLRC="$HOME/.bashrc"
 
 if [ $(id -u) -eq 0 ]; then
 	echo "Don't run as root"
- 	exit
+ 	exit 1
+fi
+
+if [ -d $HBDIR ]; then
+	echo "$HBDIR already exists, please remove and run again"
+	exit 1
 fi
 
 mkdir $HBDIR
+
+if ! command -v curl 2>&1 >/dev/null; then
+	echo "curl is needed for installation"
+	exit 1
+fi
+
+if ! command -v tar 2>&1 >/dev/null; then
+	echo "tar is needed for installation"
+	exit 1
+fi
 
 curl -L https://github.com/Homebrew/brew/tarball/master | tar -xz --strip 1 -C $HBDIR
 
 eval "$($HBDIR/bin/brew shellenv)"
 
-echo 'eval "$('$HBDIR'/bin/brew shellenv)"' >> $SHELLRC
-
 brew update --force --quiet
 
-chmod -R go-w "$(brew --prefix)/share/$SHELL"
+#QOL stuff
+chmod -R go-w "$(brew --prefix)/share/zsh"
+echo 'eval "$('$HBDIR'/bin/brew shellenv)"' >> $SHELLRC
